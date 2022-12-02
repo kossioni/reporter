217a890249b2b7e417ab603800c2b8d1d8c2dcb4
George Kossionis <101812286+kossioni@users.noreply.github.com>
2022-12-02 13:10:09 +0200

    Update main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/kossioni/reporter/grafana"
+	"github.com/kossioni/reporter/report"
 	"github.com/gorilla/mux"
 )
 
