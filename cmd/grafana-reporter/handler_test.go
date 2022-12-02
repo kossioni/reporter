48807c8253c75676e8c4b8db07ca586bb87ef660
George Kossionis <101812286+kossioni@users.noreply.github.com>
2022-12-02 13:11:03 +0200

    Update handler_test.go

--- cmd/grafana-reporter/handler_test.go
+++ cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/kossioni/reporter/grafana"
+	"github.com/kossioni/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
